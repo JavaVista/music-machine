87e153b2bf435cced547a4f915b329bbd9e4e310
Javier 'Javi' Carrion <javier.carrion@designinteractive.net>
2022-08-28 20:32:56 -0400

    feat: add drum-kit component and drum service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DrumKitComponent } from './drum-kit/drum-kit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrumKitComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/drum-kit/drum-kit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drum-kit',
+  templateUrl: './drum-kit.component.html',
+  styleUrls: ['./drum-kit.component.scss']
+})
+export class DrumKitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
